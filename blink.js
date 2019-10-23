433f1ff3219e861f05a151275ded6f711fdef2ca
Scratch <scratch@ScratchsMacBookPro.lan>
2019-10-23 22:45:27 +0800

    ok

--- blink.js
+++ blink.js
@@ -5,20 +5,18 @@ var gpin = new Gpio(1,'out');
 
 rpin.writeSync(0);
 gpin.writeSync(0);
+var rValue = 0;
+var gValue = 0;
 
 var interval = setInterval(() => {
-    var rValue = 0;
-    if (rpin.readSync() == 0) {
-        rValue = 255;
-    }
+   
+    rValue = rValue == 0 ? 255:0;
     rpin.write(rValue,() => {
         console.log("Changed R to : " + rValue);
     });
 
-    var gValue = 0;
-    if(gpin.readSync() == 0){
-        gValue = 255;
-    }
+    gValue = gValue == 0? 255:0;
+    
     gpin.write(gValue,() => {
         console.log("Changed G to : " + gValue);
     });
